ef55f10103cde6748bfa814006064114c4397833
Nuthan Vikas <nuthanvikas@slu.edu>
2025-02-10 20:46:52 -0600

    authentication functionalities

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>Stock Prediction Portal</h1>
                 <p className='text-light lead'>The Stock Prediction Portal is a platform that leverages historical stock data and machine learning models to predict future stock prices or trends. It provides users with interactive charts, real-time updates, and tools for analyzing market performance. Designed for traders and investors, it helps make data-driven decisions in the stock market.</p>
-                <Button text="Login" class="btn-outline-info"/>
+                <Button text="Explore Now" class="btn-outline-info" url="/dashboard" />
             </div>
         </div>
     </>
